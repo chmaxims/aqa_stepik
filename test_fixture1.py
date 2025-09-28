4b1afcb8fd2de486a004365a6c2ccad6f97998dc
chmaxims <max.chesn@gmail.com>
2025-09-28 22:56:26 +0300

    add new files

--- test_fixture1.py
+++ test_fixture1.py
@@ -1,26 +1,46 @@
-import pytest
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_exception1():
-    try:
-        browser = webdriver.Chrome()
-        browser.get("http://selenium1py.pythonanywhere.com/")
-        with pytest.raises(NoSuchElementException):
-            browser.find_element(By.CSS_SELECTOR, "button.btn")
-            pytest.fail("Не должно быть кнопки Отправить")
-    finally:
-        browser.quit()
+class TestMainPage1():
 
-def test_exception2():
-    try:
-        browser = webdriver.Chrome()
-        browser.get("http://selenium1py.pythonanywhere.com/")
-        with pytest.raises(NoSuchElementException):
-            browser.find_element(By.CSS_SELECTOR, "no_such_button.btn")
-            pytest.fail("Не должно быть кнопки Отправить")
-    finally:
-        browser.quit()
+    @classmethod
+    def setup_class(self):
+        print("\nstart browser for test suite..")
+        self.browser = webdriver.Chrome()
+
+    @classmethod
+    def teardown_class(self):
+        print("quit browser for test suite..")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+
+class TestMainPage2():
+
+    def setup_method(self):
+        print("start browser for test..")
+        self.browser = webdriver.Chrome()
+
+    def teardown_method(self):
+        print("quit browser for test..")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+
+
